4ea0141a1194d231fa750794f7b248c44b9273fc
informatica3 <informatica3@inacap.cl>
2026-08-26 16:13:22 -0400

    Clientes sin formato

--- Enrique_Alegria/reclutamientoPersonasApp/views.py
+++ Enrique_Alegria/reclutamientoPersonasApp/views.py
@@ -7,34 +7,43 @@ def index(request):
 def clientes(request):
     data = {
             'titulo':'Egresados',
-            'nombre1': '',
+            'nombre1': 'Leandro de Miguel',
             'categoria1':'',
             'desc1':'',
-            'nombre2': '',
+
+            'nombre2': 'Guillermina Miguez',
             'categoria2':'',
             'desc2':'',
-            'nombre3': '',
+            
+            'nombre3': 'Sheila Diallo',
             'categoria3':'',
             'desc3':'',
-            'nombre4':'',
+
+            'nombre4':'Jessica Molina',
             'categoria4':'',
             'desc4':'',
-            'nombre5':'',
+            
+            'nombre5':'Denis Freire',
             'categoria5':'',
             'desc5':'',
-            'nombre6':'',
+
+            'nombre6':'Luz-Maria Mariño',
             'categoria6':'',
             'desc6':'',
-            'nombre7':'',
+
+            'nombre7':'Maria-Ester Aguilar',
             'categoria7':'',
             'desc7':'',
-            'nombre8':'',
+
+            'nombre8':'Gerardo Alvarez',
             'categoria8':'',
             'desc8':'',
-            'nombre9':'',
+
+            'nombre9':'Irune Vazquez',
             'categoria9':'',
             'desc9':'',
-            'nombre10':'',
+            
+            'nombre10':'Soledad Llanos',
             'categoria10':'',
             'desc10':'',
         }
@@ -46,24 +55,31 @@ def egresados(request):
             'nombre1': '',
             'categoria1':'',
             'desc1':'',
+
             'nombre2': '',
             'categoria2':'',
             'desc2':'',
+
             'nombre3': '',
             'categoria3':'',
             'desc3':'',
+
             'nombre4':'',
             'categoria4':'',
             'desc4':'',
+
             'nombre5':'',
             'categoria5':'',
             'desc5':'',
+
             'nombre6':'',
             'categoria6':'',
             'desc6':'',
+
             'nombre7':'',
             'categoria7':'',
             'desc7':'',
+
             'nombre8':'',
             'categoria8':'',
             'desc8':'',
@@ -76,15 +92,19 @@ def personal(request):
         'nombre1': '',
         'categoria1':'',
         'desc1':'',
+
         'nombre2': '',
         'categoria2':'',
         'desc2':'',
+
         'nombre3': '',
         'categoria3':'',
         'desc3':'',
+
         'nombre4':'',
         'categoria4':'',
         'desc4':'',
+        
         'nombre5':'',
         'categoria5':'',
         'desc5':'',
